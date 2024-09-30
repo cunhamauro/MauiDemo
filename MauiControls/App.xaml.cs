5b5953228706b3b8cb754768d63825bc44dfa561
Mauro Cunha <cunhamauro@outlook.pt>
2024-09-30 13:50:23 +0100

    maui controls

---
+++ MauiControls/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiControls.Pages;
+
+namespace MauiControls
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new MauiPresentationControls());
+        }
+    }
+}
---
+++ MauiControls/Pages/MauiPresentationControls.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiControls.Pages;
+
+public partial class MauiPresentationControls : ContentPage
+{
+	public MauiPresentationControls()
+	{
+		InitializeComponent();
+	}
+}
